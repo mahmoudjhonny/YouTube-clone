67f57693f55077310c227e2b8ed1db276fe1f5e4
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2022-09-04 11:43:40 +0200

    Add video Cards

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,16 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
+import { VideoCard, ChannelCard } from "./index";
 
 const Videos = ({ videos }) => {
   console.log(videos);
-  return <div>bbb</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="center" gap={2}>
+      {videos.map((video, index) => (
+        <Box key={index}>{video.id.videoId && <VideoCard video={video} />}</Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
