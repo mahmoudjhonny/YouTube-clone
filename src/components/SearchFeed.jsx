5549831bc48f200c0c264cd10d337417211591c6
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2022-09-11 20:29:13 +0200

    finish search bar functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Videos } from "./index";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setvideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setvideos(data.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} color="white">
+        Search Results for:{" "}
+        <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
